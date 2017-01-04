31cbe5041e1bfc4dad2466b4e70ae15cc7b1ede3
evsantor <evsantorinaiou@gmail.com>
2017-01-05 01:16:37 +0200

    still thingss to doo

--- app/src/main/java/com/rocketpowerteam/reallysmartphone/CallContact.java
+++ app/src/main/java/com/rocketpowerteam/reallysmartphone/CallContact.java
@@ -5,55 +5,70 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
 
-public class CallContact {
+import java.util.ArrayList;
+
+
+public class CallContact extends Activity implements View.OnClickListener{
 
     public CallContact(){
+        speakOut();
+    }
 
+    public void speakOut(){
+        tts.speak(getString(R.string.ask_contact_to_call),TextToSpeech.QUEUE_FLUSH,null);
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && (resultCode == RESULT_OK)) {
+            makeCall(data);
+        }
+        else if(resultCode == RESULT_CANCELED){
+            tts.speak(getString(R.string.wrong_input), TextToSpeech.QUEUE_FLUSH, null);
+        }
 
-    private void makeCall() {
-//        Uri number = Uri.parse("tel:123456789");
-//        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
-//        startActivity(callIntent);
-//        tts.speak(getString(R.string.ask_contact_name), TextToSpeech.QUEUE_FLUSH,null);
-//        btn = (Button) findViewById(R.id.talktome);
-//        btn.setOnClickListener(this);
-//        Log.i("call2", result);
-//        ContentResolver cr = getContentResolver();
-//        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-//                null, null, null, null);
-//
-//        if (cur.getCount() > 0) {
-//            while (cur.moveToNext()) {
-//                String id = cur.getString(
-//                        cur.getColumnIndex(ContactsContract.Contacts._ID));
-//                String name = cur.getString(cur.getColumnIndex(
-//                        ContactsContract.Contacts.DISPLAY_NAME));
-//                if (name. result) {
-//                    String phoneNo = "0";
-//                    if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-//                        Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-//                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-//                                new String[]{id}, null);
-//                        while (pCur.moveToNext()) {
-//                            phoneNo = pCur.getString(pCur.getColumnIndex(
-//                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
-//
-//                        }
-//                        pCur.close();
-//                    }
-//                    Uri number = Uri.parse(phoneNo);
-//                    Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
-//                    startActivity(callIntent);
-//                }
-//            }
-//        }
+    }
+
+    private void makeCall(Intent data) {
+
+        ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+        String phoneNo = "";
+        for(String s:results){
+            Log.d(s, s);
+            if (cur.getCount() > 0) {
+                while (cur.moveToNext()) {
+                    String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    if (name.equals(s)) {
+                        if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                            Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+                            while (pCur.moveToNext()) {
+                                phoneNo = pCur.getString(pCur.getColumnIndex(
+                                          ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                            }
+                            pCur.close();
+                            Uri number = Uri.parse(phoneNo);
+                            Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                            startActivity(callIntent);
+                            return;
+                        }
+
+                    }
+                }
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/rocketpowerteam/reallysmartphone/MainActivity.java
+++ app/src/main/java/com/rocketpowerteam/reallysmartphone/MainActivity.java
@@ -1,133 +1,77 @@
 package com.rocketpowerteam.reallysmartphone;
 
-import android.content.ContentResolver;
-import android.net.Uri;
-import android.provider.ContactsContract;
 import android.content.Intent;
-import android.database.Cursor;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
-import android.support.v4.app.LoaderManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
-import static android.provider.ContactsContract.*;
-
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    Button btn;
-    final int check = 111;
-    final int get_contact_name =112;
-    TextToSpeech tts;
+public class MainActivity extends Activity implements View.OnClickListener{
 
     enum MenuItem {
-        ADD_CONTACT(1, "add contact"), CALL_CONTACT(2, "call"), PLAY_MUSIC(3, "play music"),
-        READ_MESSAGE(4, "read message"), COMPOSE_MESSAGE(5, "create compose message");
-        int code;
+        ADD_CONTACT("add"), CALL_CONTACT("call"), PLAY_MUSIC("play music"),
+        READ_MESSAGE("read"), COMPOSE_MESSAGE("create compose");
         String strCommand;
-        MenuItem(int code, String strCommand){
-            this.code = code;
+        MenuItem(String strCommand){
             this.strCommand = strCommand;
         }
-
-        public String getStrCommand() {
+        public String getDetail() {
             return strCommand;
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.talktome);
-        btn.setOnClickListener(this);
-        tts = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener(){
-
-            @Override
-            public void onInit(int status) {
-                if(status == TextToSpeech.SUCCESS){
-                    int res = tts.setLanguage(Locale.ENGLISH);
-                    if(res == TextToSpeech.LANG_NOT_SUPPORTED || res == TextToSpeech.LANG_MISSING_DATA){
-                        Log.e("TTS", "Language is not supported");
-                    }
-                }else{
-                    Log.e("TTS","Init failed");
-                }
-            }
-        });
-        tts.setSpeechRate(0.8f);
-    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-
-        //δεν εχω πολυκαταλαβει νομιζω ακομη τι παιζει με τα codes, στην startActivityForResult καλείται
-        //η onActivityResults η οποια εχει για requestCode αυτο το τσεκ που χουμε βαλει εμεις, και το
-        //result code dimiourgeitai automata. opote blepoume an aytopoy dimiourghuhke einai ok kai meta
-        //tsekaroyme se poia leitoyrgia anaferetai? ma ayto den to kseroume apo prin...an ta exw katalavei kala mexri edv
-        //skeftomoun na psaxname apla an yparxei kapoia ap tis lekseis px "add, new, contact" και να λεγαμε
-        // τοτε οτι ειμαστε στην περιπτωση 1 πχ, γιατι απο πριν δεν το ξερουμε, αυτό
-        if(resultCode == RESULT_OK){
-            switch (requestCode){
-                case check:
-                    find_menu_action(data);
-                    break;
-                case get_contact_name:
-                    break;
-
-            }
-
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && (resultCode == RESULT_OK)) {
+            find_menu_action(data);
+        }
+        else if(resultCode == RESULT_CANCELED){
+            tts.speak(getString(R.string.wrong_input), TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     private void find_menu_action(Intent data){
         ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
         for(String s:results){
             Log.i("res", s);
-            if(s.toUpperCase().equals(MenuItem.ADD_CONTACT)){
-                //create CallContact object etc
+            if(checkCommand(s.toLowerCase(), MenuItem.CALL_CONTACT.getDetail())){
+                Log.i("menu"," call contact ");
+                CallContact c = new CallContact();
+                break;
+            }else if(checkCommand(s.toLowerCase(), MenuItem.ADD_CONTACT.getDetail().toLowerCase())){
                 Log.i("menu"," add contact ");
                 break;
-            }else if(s.toUpperCase().equals((MenuItem.CALL_CONTACT))){
-                Log.i("menu"," call contact ");
-                break;
-            }else if(s.toUpperCase().equals((MenuItem.PLAY_MUSIC))){
+            }else if(checkCommand(s.toLowerCase(), MenuItem.PLAY_MUSIC.getDetail())){
                 Log.i("menu"," play music ");
                 break;
-            }else if(s.toUpperCase().equals(MenuItem.READ_MESSAGE)){
+            }else if(checkCommand(s.toLowerCase(), MenuItem.READ_MESSAGE.getDetail())){
                 Log.i("menu"," read message");
                 break;
-            }else if(s.toUpperCase().equals(MenuItem.COMPOSE_MESSAGE)){
+            }else if(checkCommand(s.toLowerCase(), MenuItem.COMPOSE_MESSAGE.getDetail())){
                 Log.i("menu"," compose message ");
                 break;
             }else{
+                Log.i("else", s);
                 continue;
             }
         }
     }
 
+    public boolean checkCommand(String str1,String str2){
+        String[] s1 = str1.split(" ");
+        String[] s2 = str2.split(" ");
 
-    @Override
-    public void onClick(View view) {
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"en-US");
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Tap and talk baby!");
-        startActivityForResult(i, check);
+        for(int i = 0; i < s1.length; i++){
+            for(int j = 0; j < s2.length; j++){
+                if(s1[i].equals(s2[j]))
+                    return true;
+            }
+        }
+        return false;
     }
+
 }
