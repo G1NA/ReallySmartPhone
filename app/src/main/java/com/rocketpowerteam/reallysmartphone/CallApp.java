0a1459b1de4c21e42bf8d636b201c76ce6bb7c8a
evsantor <evsantorinaiou@gmail.com>
2017-01-08 01:51:50 +0200

    still does not call

--- app/src/main/java/com/rocketpowerteam/reallysmartphone/CallApp.java
+++ app/src/main/java/com/rocketpowerteam/reallysmartphone/CallApp.java
@@ -1,21 +1,18 @@
 package com.rocketpowerteam.reallysmartphone;
 
-import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import android.content.ActivityNotFoundException;
+import android.widget.Button;
 
-public class CallApp{
+public class CallApp {
 
     final AppCompatActivity act;
     String name;
@@ -27,46 +24,40 @@ public class CallApp{
     }
 
     public boolean makeCall(ArrayList<String> results) {
-        String id;
+
         String con_name;
-        ContentResolver cr = act.getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
-        String phoneNo = "";
+        Cursor cur = act.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+        if(cur == null)
+            return false;
+        String phoneNo = "55555";
 
         for(String s:results) {
             Log.d("NAME", s);
             if (cur.getCount() > 0) {
+                cur.moveToFirst();
                 while (cur.moveToNext()) {
-                    id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
                     con_name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                    Log.d(con_name, name);
+                    phoneNo = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                     if (name.toLowerCase().equals(con_name.toLowerCase())) {
-                        Log.d("equals_names", name);
-                        if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                            Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                                    null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
-                            while (pCur.moveToNext()) {
-                                phoneNo = pCur.getString(pCur.getColumnIndex(
-                                        ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            } //TODO dn exw katalavei akrivws ti ginetai alla m fainetai oti pairneis ola ta id?
-                            //giati dn psaxneis mono gia to id tou contact pou eixes tairiasma sto onoma??
-                            //http://stackoverflow.com/questions/26823578/how-to-get-phone-number-from-a-specific-contact-in-android
-                            pCur.close();
-                            Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(phoneNo));
-                            try {
-                                act.startActivity(callIntent);
-                            }
-                            catch(ActivityNotFoundException e){
-                                Log.d("catch", name);
-                                return false;
-                            }
-                        }
-                        return true;
+                        phoneNo = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        callContact(phoneNo);
                     }
                 }
+
             }
         }
         return false;
+    }
+
+    private void callContact(String phoneNo){
+        Intent callIntent = new Intent((Intent.ACTION_CALL), Uri.parse(phoneNo));
+        try {
+            act.startActivity(callIntent);
+            Log.d("it s ok","");
+        }
+        catch(ActivityNotFoundException e){
+            Log.d("catch", name);
+        }
     }
 
 }
