3623bd20190b7e20e842f89ded6e810fd0490f7f
G1NA <p3130052@dias.aueb.gr>
2017-01-09 23:35:03 +0200

    playing music fixed, date and time fixed, also contains some wannabe autostarted code.........

--- app/src/main/java/com/rocketpowerteam/reallysmartphone/AddContactApp.java
+++ app/src/main/java/com/rocketpowerteam/reallysmartphone/AddContactApp.java
@@ -2,12 +2,8 @@ package com.rocketpowerteam.reallysmartphone;
 
 import android.content.ContentProviderOperation;
 import android.content.ContentProviderResult;
-import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.OperationApplicationException;
-import android.net.Uri;
 import android.os.RemoteException;
-import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -28,66 +24,24 @@ public class AddContactApp {
     }
 
     public void addContact() {
-/*
-* ContentValues values = new ContentValues();
-            values.put(Data.RAW_CONTACT_ID, 001);
-            values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
-            values.put(Phone.NUMBER, "            1-800-GOOG-411      ");
-            values.put(Phone.TYPE, Phone.TYPE_CUSTOM);
-            values.put(Phone.LABEL, "free directory assistance");
-            Uri dataUri = getContentResolver().insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
-
-        ContentValues values = new ContentValues();
-        Uri rawContactUri = m.getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
-        long rawContactId = ContentUris.parseId(rawContactUri);
-        values.put(ContactsContract.Contacts.Data.RAW_CONTACT_ID, rawContactId);
-        values.put(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
-        values.put(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, c.getName()); //the contact name
-        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, c.getNumber()); //the contact phone number
-        values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-        m.getContentResolver().insert(android.provider.ContactsContract.Data.CONTENT_URI, values); */
-
-        /*
-        Log.i("inside add contact app", "mpla");
-        ArrayList<ContentProviderOperation> ops =
-                new ArrayList<ContentProviderOperation>();
-        int rawContactInsertIndex = ops.size();
-        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                .withValue(ContactsContract.Data.RAW_CONTACT_ID, rawContactInsertIndex)
-                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, c.getNumber())
-                .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, c.getName())
-                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM)
-                .withValue(ContactsContract.CommonDataKinds.Phone.LABEL, "free directory assistance")
-                .build());
-        Log.i("here fucking bastard", "");
-        try {
-            m.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-            Log.e("mpla","mplampla");
-        } catch (OperationApplicationException e) {
-            e.printStackTrace();
-        }
-*/
         ArrayList<ContentProviderOperation> cntProOper = new ArrayList<ContentProviderOperation>();
         int contactIndex = cntProOper.size();//ContactSize
-        Log.i("mpla","beefore");
+
         //Newly Inserted contact
         // A raw contact will be inserted ContactsContract.RawContacts table in contacts database.
-        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)//Step1
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
                 .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
                 .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
-        Log.i("first attempt", "mplo");
+
         //Display name will be inserted in ContactsContract.Data table
-        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)//Step2
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,contactIndex)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
                 .withValue(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, c.getName()) // Name of the contact
                 .build());
-        Log.i("second attempt", "");
+
         //Mobile number will be inserted in ContactsContract.Data table
-        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)//Step 3
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
                 .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,contactIndex)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
                 .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, c.getNumber()) // Number to be added
@@ -103,11 +57,11 @@ public class AddContactApp {
         }
         catch (RemoteException exp)
         {
-            Log.i("wtf","wtff");
+            exp.printStackTrace();
         }
         catch (OperationApplicationException exp)
         {
-            Log.i("wtfff","wtfda");
+            exp.printStackTrace();
         }
 
     }
